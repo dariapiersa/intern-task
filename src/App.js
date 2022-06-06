a7aef7308a46fb1cd9264db3e58cfabe72a63078
Daria Piersa <dariapiersa17@gmail.com>
2022-06-06 19:43:48 +0200

    solved tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Dot from "./Dot";
 
 function App() {
+    const [selected, setSelected] = useState(null);
+    const onSelect = (id) => {
+    setSelected(id);
+}
     return (
         <div className="dots-wrapper">
             {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
+                <Dot key={i} selected={i === selected} onSelect={onSelect} k={i}/>
             ))}
         </div>
     );
